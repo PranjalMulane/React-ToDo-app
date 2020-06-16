e645f2be2571ef0f442ec52ce67e641dc6bec339
Pranjal Mulane <pranjalsmulane@gmail.com>
2020-06-16 16:34:17 +0530

    Updates

---
+++ precache-manifest.e029f17b3dd4db3bf2a86296dd29bd45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34c90fd1733f5be364280dcb2a56a14",
+    "url": "/PranjalMulane/React-ToDo-app/index.html"
+  },
+  {
+    "revision": "0da7ebec2b103466c548",
+    "url": "/PranjalMulane/React-ToDo-app/static/css/main.bcffa30d.chunk.css"
+  },
+  {
+    "revision": "d3be3eda1be3005915cf",
+    "url": "/PranjalMulane/React-ToDo-app/static/js/2.84da8b19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PranjalMulane/React-ToDo-app/static/js/2.84da8b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da7ebec2b103466c548",
+    "url": "/PranjalMulane/React-ToDo-app/static/js/main.53dadf12.chunk.js"
+  },
+  {
+    "revision": "69c7975657e1314ebb74",
+    "url": "/PranjalMulane/React-ToDo-app/static/js/runtime-main.cfdcc04b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PranjalMulane/React-ToDo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
